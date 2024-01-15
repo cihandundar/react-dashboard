f495bac89a6800906134b924791f419c3c3adb0c
cihandundar <94462152+cihandundar@users.noreply.github.com>
2024-01-15 19:54:17 +0300

    toastify added to router structure

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Navbar, Sidebar } from "components";
 import {
   Billing,
   Dashboard,
@@ -9,19 +10,29 @@ import {
 } from "./pages";
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/tables" element={<Tables />} />
-        <Route path="/billing" element={<Billing />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="*" element={<NotFoundError />} />
-      </Routes>
+      <ToastContainer position="bottom-right" />
+      <div className="container">
+        <div>
+          <Sidebar />
+        </div>
+        <div className="wrapper">
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/tables" element={<Tables />} />
+            <Route path="/billing" element={<Billing />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/signin" element={<SignIn />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="*" element={<NotFoundError />} />
+          </Routes>
+        </div>
+      </div>
     </BrowserRouter>
   );
 }
