078de21d55cd8307c9ac3d7180ac3c04699f84e7
cihandundar <94462152+cihandundar@users.noreply.github.com>
2024-01-15 20:54:02 +0300

    navbar component created

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,9 +1,32 @@
 import React from "react";
-
+import { CgProfile } from "react-icons/cg";
+import { Link } from "react-router-dom";
+import { IoSettingsSharp } from "react-icons/io5";
+import { FaBell } from "react-icons/fa";
+import { FaSearch } from "react-icons/fa";
 const Navbar = () => {
   return (
-    <nav>
-      <div></div>
+    <nav className="nav">
+      <div className="nav__container">
+        <div className="input">
+          <span className="search">
+            <FaSearch />
+          </span>
+          <input type="text" placeholder="Type here..." />
+        </div>
+        <span>
+          <Link to="/">
+            <CgProfile />
+            Sign In
+          </Link>
+        </span>
+        <span>
+          <IoSettingsSharp />
+        </span>
+        <span>
+          <FaBell />
+        </span>
+      </div>
     </nav>
   );
 };
