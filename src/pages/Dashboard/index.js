841d84daee340e57eff0ca8f95df538574d65997
cihandundar <94462152+cihandundar@users.noreply.github.com>
2024-01-15 22:18:41 +0300

    dashboard cart component imported

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,7 +1,16 @@
+import { Cart } from "components";
 import React from "react";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <div className="dashboard">
+      <div className="dashboard__container">
+        <div className="dashbord__container__top">
+          <Cart />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
