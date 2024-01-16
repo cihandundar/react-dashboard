287025edb98a4afdac6291c2a48d5cde65d9c339
cihandundar <94462152+cihandundar@users.noreply.github.com>
2024-01-16 19:58:17 +0300

    chart imported

--- src/components/index.js
+++ src/components/index.js
@@ -2,4 +2,5 @@ import Navbar from "./Navbar";
 import Sidebar from "./Sidebar";
 import Cart from "./Cart";
 import RateChart from "./RateChart";
-export { Navbar, Sidebar, Cart, RateChart };
+import Chart from "./Chart";
+export { Navbar, Sidebar, Cart, RateChart, Chart };
