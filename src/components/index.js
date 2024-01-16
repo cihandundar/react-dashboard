ea4614df6e2577ee72600dc3da3a786699bbb816
cihandundar <94462152+cihandundar@users.noreply.github.com>
2024-01-16 23:58:01 +0300

    footer imported

--- src/components/index.js
+++ src/components/index.js
@@ -3,4 +3,6 @@ import Sidebar from "./Sidebar";
 import Cart from "./Cart";
 import RateChart from "./RateChart";
 import Chart from "./Chart";
-export { Navbar, Sidebar, Cart, RateChart, Chart };
+import ProjectsTable from "./ProjectsTable";
+import Footer from "./Footer";
+export { Navbar, Sidebar, Cart, RateChart, Chart, ProjectsTable, Footer };
