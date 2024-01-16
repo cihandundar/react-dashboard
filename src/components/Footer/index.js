2e7f6ac373a0208238e50acadb664b0ca5be92c1
cihandundar <94462152+cihandundar@users.noreply.github.com>
2024-01-16 23:57:15 +0300

    footer component created

---
+++ src/components/Footer/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer__container">
+        <div className="footer__left">@ 2024, Made with 🐺</div>
+        <div className="footer__right">
+          <ul className="footer__right__list">
+            <li>Marketplace</li>
+            <li>Blog</li>
+            <li>License</li>
+          </ul>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
