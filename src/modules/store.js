1fa138e49a63e15fda1d077708019b4c460fd3f9
cihandundar <94462152+cihandundar@users.noreply.github.com>
2024-01-17 21:29:52 +0300

    userslice imported

--- src/modules/store.js
+++ src/modules/store.js
@@ -1,10 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import logger from "redux-logger";
 import authReducer from "../features/auth/authSlice";
-
+import userReducer from "../features/users/userSlice";
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    users: userReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
