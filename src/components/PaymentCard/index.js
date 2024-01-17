d1918c85c64a0ca70d11112fdbc15fe9ebc28702
cihandundar <94462152+cihandundar@users.noreply.github.com>
2024-01-17 21:30:30 +0300

    component fix

---
+++ src/components/PaymentCard/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PaymentCard = () => {
+  return (
+    <div className="payment">
+      <div className="payment__container"></div>
+    </div>
+  );
+};
+
+export default PaymentCard;
