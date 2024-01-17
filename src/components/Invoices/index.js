f2880f7728cc3c6824dab24bb57513ff3352aec9
cihandundar <94462152+cihandundar@users.noreply.github.com>
2024-01-17 23:38:11 +0300

    invoices component created

---
+++ src/components/Invoices/index.js
@@ -0,0 +1,198 @@
+import React from "react";
+
+const Invoices = () => {
+  return (
+    <div className="invoices">
+      <div className="invoices__container">
+        <div className="invoices__container__title">
+          <h2>Invoices</h2>
+          <button>VIEW ALL</button>
+        </div>
+        <div className="invoices__container__wrapper">
+          <div className="invoices__container__box">
+            <div className="invoices__container__box__left">
+              <p>
+                <strong>March,01,2020</strong>
+              </p>
+              <span className="textSoft">
+                <p>#MS-415646</p>
+              </span>
+            </div>
+            <div className="invoices__container__box__right">
+              <div className="invoices__container__box__right__content">
+                <span className="textSoft">
+                  <p>$180</p>
+                </span>
+                <p>
+                  <span className="pdfIcon">
+                    <svg
+                      stroke="currentColor"
+                      fill="currentColor"
+                      stroke-width="0"
+                      viewBox="0 0 512 512"
+                      color="#fff"
+                      height="15px"
+                      width="15px"
+                      xmlns="http://www.w3.org/2000/svg"
+                      style={{ color: "rgb(255, 255, 255);" }}
+                    >
+                      <path d="M428 224H288a48 48 0 01-48-48V36a4 4 0 00-4-4h-92a64 64 0 00-64 64v320a64 64 0 0064 64h224a64 64 0 0064-64V228a4 4 0 00-4-4zm-92 160H176a16 16 0 010-32h160a16 16 0 010 32zm0-80H176a16 16 0 010-32h160a16 16 0 010 32z"></path>
+                      <path d="M419.22 188.59L275.41 44.78a2 2 0 00-3.41 1.41V176a16 16 0 0016 16h129.81a2 2 0 001.41-3.41z"></path>
+                    </svg>
+                  </span>
+                  PDF
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="invoices__container__box">
+            <div className="invoices__container__box__left">
+              <p>
+                <strong>February, 10, 2021</strong>
+              </p>
+              <span className="textSoft">
+                <p>#RV-126749</p>
+              </span>
+            </div>
+            <div className="invoices__container__box__right">
+              <div className="invoices__container__box__right__content">
+                <span className="textSoft">
+                  <p>$250</p>
+                </span>
+                <p>
+                  <span className="pdfIcon">
+                    <svg
+                      stroke="currentColor"
+                      fill="currentColor"
+                      stroke-width="0"
+                      viewBox="0 0 512 512"
+                      color="#fff"
+                      height="15px"
+                      width="15px"
+                      xmlns="http://www.w3.org/2000/svg"
+                      style={{ color: "rgb(255, 255, 255);" }}
+                    >
+                      <path d="M428 224H288a48 48 0 01-48-48V36a4 4 0 00-4-4h-92a64 64 0 00-64 64v320a64 64 0 0064 64h224a64 64 0 0064-64V228a4 4 0 00-4-4zm-92 160H176a16 16 0 010-32h160a16 16 0 010 32zm0-80H176a16 16 0 010-32h160a16 16 0 010 32z"></path>
+                      <path d="M419.22 188.59L275.41 44.78a2 2 0 00-3.41 1.41V176a16 16 0 0016 16h129.81a2 2 0 001.41-3.41z"></path>
+                    </svg>
+                  </span>
+                  PDF
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="invoices__container__box">
+            <div className="invoices__container__box__left">
+              <p>
+                <strong>April, 05, 2020</strong>
+              </p>
+              <span className="textSoft">
+                <p>#QW-103578</p>
+              </span>
+            </div>
+            <div className="invoices__container__box__right">
+              <div className="invoices__container__box__right__content">
+                <span className="textSoft">
+                  <p>$120</p>
+                </span>
+                <p>
+                  <span className="pdfIcon">
+                    <svg
+                      stroke="currentColor"
+                      fill="currentColor"
+                      stroke-width="0"
+                      viewBox="0 0 512 512"
+                      color="#fff"
+                      height="15px"
+                      width="15px"
+                      xmlns="http://www.w3.org/2000/svg"
+                      style={{ color: "rgb(255, 255, 255);" }}
+                    >
+                      <path d="M428 224H288a48 48 0 01-48-48V36a4 4 0 00-4-4h-92a64 64 0 00-64 64v320a64 64 0 0064 64h224a64 64 0 0064-64V228a4 4 0 00-4-4zm-92 160H176a16 16 0 010-32h160a16 16 0 010 32zm0-80H176a16 16 0 010-32h160a16 16 0 010 32z"></path>
+                      <path d="M419.22 188.59L275.41 44.78a2 2 0 00-3.41 1.41V176a16 16 0 0016 16h129.81a2 2 0 001.41-3.41z"></path>
+                    </svg>
+                  </span>
+                  PDF
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="invoices__container__box">
+            <div className="invoices__container__box__left">
+              <p>
+                <strong>June, 25, 2019</strong>
+              </p>
+              <span className="textSoft">
+                <p>#MS-415646</p>
+              </span>
+            </div>
+            <div className="invoices__container__box__right">
+              <div className="invoices__container__box__right__content">
+                <span className="textSoft">
+                  <p>$180</p>
+                </span>
+                <p>
+                  <span className="pdfIcon">
+                    <svg
+                      stroke="currentColor"
+                      fill="currentColor"
+                      stroke-width="0"
+                      viewBox="0 0 512 512"
+                      color="#fff"
+                      height="15px"
+                      width="15px"
+                      xmlns="http://www.w3.org/2000/svg"
+                      style={{ color: "rgb(255, 255, 255);" }}
+                    >
+                      <path d="M428 224H288a48 48 0 01-48-48V36a4 4 0 00-4-4h-92a64 64 0 00-64 64v320a64 64 0 0064 64h224a64 64 0 0064-64V228a4 4 0 00-4-4zm-92 160H176a16 16 0 010-32h160a16 16 0 010 32zm0-80H176a16 16 0 010-32h160a16 16 0 010 32z"></path>
+                      <path d="M419.22 188.59L275.41 44.78a2 2 0 00-3.41 1.41V176a16 16 0 0016 16h129.81a2 2 0 001.41-3.41z"></path>
+                    </svg>
+                  </span>
+                  PDF
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="invoices__container__box">
+            <div className="invoices__container__box__left">
+              <p>
+                <strong>March, 01, 2019</strong>
+              </p>
+              <span className="textSoft">
+                <p>#AR-803481</p>
+              </span>
+            </div>
+            <div className="invoices__container__box__right">
+              <div className="invoices__container__box__right__content">
+                <span className="textSoft">
+                  <p>$300</p>
+                </span>
+                <p>
+                  <span className="pdfIcon">
+                    <svg
+                      stroke="currentColor"
+                      fill="currentColor"
+                      stroke-width="0"
+                      viewBox="0 0 512 512"
+                      color="#fff"
+                      height="15px"
+                      width="15px"
+                      xmlns="http://www.w3.org/2000/svg"
+                      style={{ color: "rgb(255, 255, 255);" }}
+                    >
+                      <path d="M428 224H288a48 48 0 01-48-48V36a4 4 0 00-4-4h-92a64 64 0 00-64 64v320a64 64 0 0064 64h224a64 64 0 0064-64V228a4 4 0 00-4-4zm-92 160H176a16 16 0 010-32h160a16 16 0 010 32zm0-80H176a16 16 0 010-32h160a16 16 0 010 32z"></path>
+                      <path d="M419.22 188.59L275.41 44.78a2 2 0 00-3.41 1.41V176a16 16 0 0016 16h129.81a2 2 0 001.41-3.41z"></path>
+                    </svg>
+                  </span>
+                  PDF
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Invoices;
