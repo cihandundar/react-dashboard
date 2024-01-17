cefc3459f70b768147ecb4d5f22e6805916bed59
cihandundar <94462152+cihandundar@users.noreply.github.com>
2024-01-17 23:14:00 +0300

    payment component imported

--- src/pages/Billing/index.js
+++ src/pages/Billing/index.js
@@ -1,7 +1,14 @@
+import { PaymentCard } from "components";
 import React from "react";
 
 const Billing = () => {
-  return <div>Billing</div>;
+  return (
+    <div className="billing">
+      <div className="billing__container">
+        <PaymentCard />
+      </div>
+    </div>
+  );
 };
 
 export default Billing;
