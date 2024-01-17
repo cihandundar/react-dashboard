6f9d36d7507d9728ff4cae9c4401662ae4ccd4f5
cihandundar <94462152+cihandundar@users.noreply.github.com>
2024-01-18 01:49:59 +0300

    The route structure was rebuilt from scratch

--- src/App.js
+++ src/App.js
@@ -12,7 +12,44 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import PrivateRoute from "components/PrivateRoute";
 function App() {
+  const route = [
+    {
+      path: "/signin",
+      element: <SignIn />,
+    },
+    {
+      path: "/signup",
+      element: <SignUp />,
+    },
+    {
+      path: "*",
+      element: <NotFoundError />,
+    },
+    {
+      element: <PrivateRoute />,
+      routes: [
+        {
+          path: "/",
+          element: <Dashboard />,
+        },
+        {
+          path: "/tables",
+          element: <Tables />,
+        },
+        {
+          path: "/billing",
+          element: <Billing />,
+        },
+        {
+          path: "/profile",
+          element: <Profile />,
+        },
+      ],
+    },
+  ];
+
   return (
     <BrowserRouter>
       <ToastContainer position="bottom-right" />
@@ -23,13 +60,24 @@ function App() {
         <section className="wrapper">
           <Navbar />
           <Routes>
-            <Route path="/" element={<Dashboard />} />
-            <Route path="/signin" element={<SignIn />} />
-            <Route path="/signup" element={<SignUp />} />
-            <Route path="/tables" element={<Tables />} />
-            <Route path="/billing" element={<Billing />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="*" element={<NotFoundError />} />
+            {route.map((route, index) => {
+              if (route.routes) {
+                return (
+                  <Route element={route.element} key={index}>
+                    {route.routes.map((subRoute, subIndex) => (
+                      <Route
+                        element={subRoute.element}
+                        path={subRoute.path}
+                        key={subIndex}
+                      />
+                    ))}
+                  </Route>
+                );
+              }
+              return (
+                <Route key={index} path={route.path} element={route.element} />
+              );
+            })}
           </Routes>
           <Footer />
         </section>
