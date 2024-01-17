92df0b8eba152eb8e0f538dd556b7eb3c27cdda4
cihandundar <94462152+cihandundar@users.noreply.github.com>
2024-01-17 21:30:22 +0300

    AuthorsTable component was created and information was added

---
+++ src/components/AuthorsTable/index.js
@@ -0,0 +1,54 @@
+import { fetchUser } from "features/users/userSlice";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+const AuthorsTable = () => {
+  const users = useSelector((state) => state?.users?.item);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchUser());
+  }, [dispatch]);
+
+  return (
+    <div className="table">
+      <div className="table__container">
+        <table border="1" style={{ borderCollapse: "collapse", width: "100%" }}>
+          <thead>
+            <tr>
+              <th>AUTHOR</th>
+              <th>FUNCTION</th>
+              <th>STATUS</th>
+              <th>EMPLOYED</th>
+              <th>ACTION</th>
+            </tr>
+          </thead>
+          <tbody className="tbodyWrapper">
+            {users?.map((userItem) => (
+              <tr key={userItem?.id}>
+                <td>
+                  <div className="tableTitle">
+                    <span className="tableAvatar">
+                      <img src={userItem.avatar} alt="" />
+                    </span>
+                    <div className="tableWrapper">
+                      <div>
+                        <h5>{userItem?.name}</h5>
+                      </div>
+                      <p>{userItem?.email}</p>
+                    </div>
+                  </div>
+                </td>
+                <td>{userItem?.job}</td>
+                <td>{userItem?.info}</td>
+                <td>{userItem?.history}</td>
+                <td>Edit</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default AuthorsTable;
