510741265a94932ca682cda1310b8ca4f7fec867
cihandundar <94462152+cihandundar@users.noreply.github.com>
2024-01-18 18:31:02 +0300

    Profile information component was created

---
+++ src/components/ProfileInformation/index.js
@@ -0,0 +1,120 @@
+import React from "react";
+import { FaArrowRight } from "react-icons/fa6";
+import greenIcon from "../../assets/greenIcon.svg";
+import car from "../../assets/car.svg";
+const ProfileInformation = () => {
+  return (
+    <div className="profileinformation">
+      <div className="profileinformation__container">
+        <div className="profileinformation__container__wrapper">
+          <div className="welcome">
+            <div className="welcome__top">
+              <h2>Welcome back!</h2>
+              <p>Nice to see you, Mark Johnson!</p>
+            </div>
+            <div className="welcome__bottom">
+              <p>
+                Tap to record
+                <span className="arrow">
+                  <FaArrowRight />
+                </span>
+              </p>
+            </div>
+          </div>
+          <div className="carinformation">
+            <div className="carinformation__top">
+              <h2>Car Informations</h2>
+              <p>Hello, Mark Johnson! Your Car is ready.</p>
+            </div>
+            <div className="carinformation__center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="85 85 170 170"
+                style={{ transform: "rotate(-90deg); overflow: visible;" }}
+              >
+                <linearGradient
+                  id="grd_j7fpdo6doe37"
+                  x1="0%"
+                  y1="0%"
+                  x2="0%"
+                  y2="100%"
+                  gradientTransform="rotate(90, .5, .5)"
+                >
+                  <stop offset="0" stop-color="rgba(5, 205, 153, 0)"></stop>
+                  <stop offset="100" stop-color="#05CD99"></stop>
+                </linearGradient>
+                <circle
+                  cx="170"
+                  cy="170"
+                  r="77.5"
+                  stroke="transparent"
+                  stroke-width="15"
+                  fill="#080D25"
+                ></circle>
+                <circle
+                  cx="170"
+                  cy="170"
+                  r="77.5"
+                  fill="none"
+                  stroke-width="15"
+                  stroke-dasharray="486.94686130641793"
+                  stroke-dashoffset="146.08405839192534"
+                  stroke-linecap="round"
+                  stroke="url(#grd_j7fpdo6doe37)"
+                  style={{ transition: " stroke-dashoffset 400ms ease 0s;" }}
+                ></circle>
+              </svg>
+              <div className="carinformation__center__text">
+                <img src={greenIcon} alt="" />
+                <h2>68%</h2>
+                <p>Current Load</p>
+              </div>
+            </div>
+            <div className="carinformation__center__bottom">
+              <h2>0h 58min</h2>
+              <p>Time to full charge</p>
+            </div>
+            <div className="carinformation__bottom">
+              <div className="carinformation__bottom__wrapper">
+                <div className="carinformation__bottom__wrapper__box">
+                  <div className="carinformation__bottom__wrapper__box__left">
+                    <p>Battery Health</p>
+                    <h3>76%</h3>
+                  </div>
+                  <div className="carinformation__bottom__wrapper__box__right">
+                    <img src={car} alt="" />
+                  </div>
+                </div>
+                <div className="carinformation__bottom__wrapper__box">
+                  <div>
+                    <p>Efficiency</p>
+                    <h3>+20%</h3>
+                  </div>
+                </div>
+              </div>
+              <div className="carinformation__bottom__wrapper">
+                <div className="carinformation__bottom__wrapper__box">
+                  <div className="carinformation__bottom__wrapper__box__left">
+                    <p>Battery Health</p>
+                    <h3>76%</h3>
+                  </div>
+                  <div className="carinformation__bottom__wrapper__box__right">
+                    <img src={car} alt="" />
+                  </div>
+                </div>
+                <div className="carinformation__bottom__wrapper__box">
+                  <div>
+                    <p>This Week</p>
+                    <h3>1.342km</h3>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProfileInformation;
