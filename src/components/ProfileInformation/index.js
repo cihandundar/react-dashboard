11688683f3468ae522a474a2aef76819d25c7f6e
cihandundar <94462152+cihandundar@users.noreply.github.com>
2024-01-18 19:36:43 +0300

    style fix

--- src/components/ProfileInformation/index.js
+++ src/components/ProfileInformation/index.js
@@ -2,6 +2,9 @@ import React from "react";
 import { FaArrowRight } from "react-icons/fa6";
 import greenIcon from "../../assets/greenIcon.svg";
 import car from "../../assets/car.svg";
+import { FaFacebookSquare } from "react-icons/fa";
+import { IoLogoTwitter } from "react-icons/io";
+import { FaInstagram } from "react-icons/fa6";
 const ProfileInformation = () => {
   return (
     <div className="profileinformation">
@@ -23,47 +26,49 @@ const ProfileInformation = () => {
           </div>
           <div className="carinformation">
             <div className="carinformation__top">
-              <h2>Car Informations</h2>
+              <h3>Car Informations</h3>
               <p>Hello, Mark Johnson! Your Car is ready.</p>
             </div>
             <div className="carinformation__center">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="85 85 170 170"
-                style={{ transform: "rotate(-90deg); overflow: visible;" }}
-              >
-                <linearGradient
-                  id="grd_j7fpdo6doe37"
-                  x1="0%"
-                  y1="0%"
-                  x2="0%"
-                  y2="100%"
-                  gradientTransform="rotate(90, .5, .5)"
+              <div className="carinformation__center__svg">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="85 85 170 170"
+                  style={{ transform: "rotate(-90deg); overflow: visible;" }}
                 >
-                  <stop offset="0" stop-color="rgba(5, 205, 153, 0)"></stop>
-                  <stop offset="100" stop-color="#05CD99"></stop>
-                </linearGradient>
-                <circle
-                  cx="170"
-                  cy="170"
-                  r="77.5"
-                  stroke="transparent"
-                  stroke-width="15"
-                  fill="#080D25"
-                ></circle>
-                <circle
-                  cx="170"
-                  cy="170"
-                  r="77.5"
-                  fill="none"
-                  stroke-width="15"
-                  stroke-dasharray="486.94686130641793"
-                  stroke-dashoffset="146.08405839192534"
-                  stroke-linecap="round"
-                  stroke="url(#grd_j7fpdo6doe37)"
-                  style={{ transition: " stroke-dashoffset 400ms ease 0s;" }}
-                ></circle>
-              </svg>
+                  <linearGradient
+                    id="grd_j7fpdo6doe37"
+                    x1="0%"
+                    y1="0%"
+                    x2="0%"
+                    y2="100%"
+                    gradientTransform="rotate(90, .5, .5)"
+                  >
+                    <stop offset="0" stop-color="rgba(5, 205, 153, 0)"></stop>
+                    <stop offset="100" stop-color="#05CD99"></stop>
+                  </linearGradient>
+                  <circle
+                    cx="170"
+                    cy="170"
+                    r="77.5"
+                    stroke="transparent"
+                    stroke-width="15"
+                    fill="#080D25"
+                  ></circle>
+                  <circle
+                    cx="170"
+                    cy="170"
+                    r="77.5"
+                    fill="none"
+                    stroke-width="15"
+                    stroke-dasharray="486.94686130641793"
+                    stroke-dashoffset="146.08405839192534"
+                    stroke-linecap="round"
+                    stroke="url(#grd_j7fpdo6doe37)"
+                    style={{ transition: " stroke-dashoffset 400ms ease 0s;" }}
+                  ></circle>
+                </svg>
+              </div>
               <div className="carinformation__center__text">
                 <img src={greenIcon} alt="" />
                 <h2>68%</h2>
@@ -108,6 +113,45 @@ const ProfileInformation = () => {
                     <h3>1.342km</h3>
                   </div>
                 </div>
+              </div>
+            </div>
+          </div>
+          <div className="profileinformation__bottom">
+            <div className="profileinformation__bottom__title">
+              <h3>Profile Information</h3>
+              <p>
+                Hi, I’m Mark Johnson, Decisions: If you can’t decide, the answer
+                is no. If two equally difficult paths, choose the one more
+                painful in the short term (pain avoidance is creating an
+                illusion of equality).
+              </p>
+            </div>
+            <div className="profileinformation__bottom__center">
+              <p>
+                <span className="centerSoft">Full Name: </span>Mark Johnson
+              </p>
+              <p>
+                <span className="centerSoft">Mobile:</span> (44) 123 1234 123
+              </p>
+              <p>
+                <span className="centerSoft">Email:</span> mark@simmmple.com
+              </p>
+              <p>
+                <span className="centerSoft">Location:</span> United States
+              </p>
+              <div className="profileinformation__bottom__socialmedia">
+                <p>
+                  <span className="centerSoft">Social:</span>
+                  <span className="socialmediaIcon">
+                    <FaFacebookSquare />
+                  </span>
+                  <span className="socialmediaIcon">
+                    <IoLogoTwitter />
+                  </span>
+                  <span className="socialmediaIcon">
+                    <FaInstagram />
+                  </span>
+                </p>
               </div>
             </div>
           </div>
