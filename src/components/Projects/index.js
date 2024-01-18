2f8237d33f6fe2e170cff11b659812f6308226f8
cihandundar <94462152+cihandundar@users.noreply.github.com>
2024-01-18 21:11:21 +0300

    style fix

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -11,94 +11,52 @@ const Projects = () => {
           <span className="textSoft">Architects design houses</span>
         </div>
         <div className="projects__container__wrapper">
-          <div className="projects__container__box">
-            <div className="project__container__box__img">
+          <div className="projects__box">
+            <div className="projects__box__img">
               <img src={project1} alt="" />
             </div>
-            <div className="project__container__box__text">
+            <div className="projects__box__text">
               <span className="textSoft">Project#1</span>
-              <h4 style={{ marginBlock: "10px" }}>Modern</h4>
+              <h4>Modern</h4>
               <span className="textSoft">
                 As Uber works through a huge amount of internal management
                 turmoil.
               </span>
             </div>
-            <div
-              className="project__container__box__btn"
-              style={{ marginBlock: "10px" }}
-            >
-              <button
-                style={{
-                  background: "transparent",
-                  border: "1px solid grey",
-                  padding: "10px 20px",
-                  color: "white",
-                  borderRadius: "10px",
-                  cursor: "pointer",
-                }}
-              >
-                VIEW ALL
-              </button>
+            <div className="projects__box__btn">
+              <button>VIEW ALL</button>
             </div>
           </div>
-          <div className="projects__container__box">
-            <div className="project__container__box__img">
+          <div className="projects__box">
+            <div className="projects__box__img">
               <img src={project2} alt="" />
             </div>
-            <div className="project__container__box__text">
+            <div className="projects__box__text">
               <span className="textSoft">Project#2</span>
-              <h4 style={{ marginBlock: "10px" }}>Scandinavian</h4>
+              <h4>Scandinavian</h4>
               <span className="textSoft">
                 As Uber works through a huge amount of internal management
                 turmoil.
               </span>
             </div>
-            <div
-              className="project__container__box__btn"
-              style={{ marginBlock: "10px" }}
-            >
-              <button
-                style={{
-                  background: "transparent",
-                  border: "1px solid grey",
-                  padding: "10px 20px",
-                  color: "white",
-                  borderRadius: "10px",
-                  cursor: "pointer",
-                }}
-              >
-                VIEW ALL
-              </button>
+            <div className="projects__box__btn">
+              <button>VIEW ALL</button>
             </div>
           </div>
-          <div className="projects__container__box">
-            <div className="project__container__box__img">
+          <div className="projects__box">
+            <div className="projects__box__img">
               <img src={project3} alt="" />
             </div>
-            <div className="project__container__box__text">
+            <div className="projects__box__text">
               <span className="textSoft">Project#3</span>
-              <h4 style={{ marginBlock: "10px" }}>Minimalist</h4>
+              <h4 s>Minimalist</h4>
               <span className="textSoft">
                 As Uber works through a huge amount of internal management
                 turmoil.
               </span>
             </div>
-            <div
-              className="project__container__box__btn"
-              style={{ marginBlock: "10px" }}
-            >
-              <button
-                style={{
-                  background: "transparent",
-                  border: "1px solid grey",
-                  padding: "10px 20px",
-                  color: "white",
-                  borderRadius: "10px",
-                  cursor: "pointer",
-                }}
-              >
-                VIEW ALL
-              </button>
+            <div className="projects__box__btn">
+              <button>VIEW ALL</button>
             </div>
           </div>
         </div>
